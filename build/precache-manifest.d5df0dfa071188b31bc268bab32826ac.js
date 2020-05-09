436f9e3297948d1aa9f6a78039eafa4fed8e1040
Tekena <tekipeps@localhost.localdomain>
2020-05-09 02:53:01 +0100

    fixed delete error

---
+++ build/precache-manifest.d5df0dfa071188b31bc268bab32826ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a436c1aad960a780a293672ae8eea26c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d0a8089d02775423a0f",
+    "url": "/static/css/main.b5adea53.chunk.css"
+  },
+  {
+    "revision": "d34ffa71c28263307371",
+    "url": "/static/js/2.14ec9f19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.14ec9f19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d0a8089d02775423a0f",
+    "url": "/static/js/main.745500de.chunk.js"
+  },
+  {
+    "revision": "dde6df0ed59d1126e71d",
+    "url": "/static/js/runtime-main.3c1de91f.js"
+  }
+]);
