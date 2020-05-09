47c759729811f24ac8cbf4674f86dd061e89bea8
Tekena <tekipeps@localhost.localdomain>
2020-05-09 02:31:21 +0100

    added build

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express();
 
+
 app.use(express.json());
 app.use(cors())
-
+app.use(express.static('build'))
 morgan.token('body', (req, res) => {
   return JSON.stringify(req.body)
 })
