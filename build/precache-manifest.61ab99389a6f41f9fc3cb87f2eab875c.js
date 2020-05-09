a511f66159cd8aa4ee717757c641855ac76dc932
Tekena <tekipeps@localhost.localdomain>
2020-05-09 02:35:34 +0100

    fixed url

---
+++ build/precache-manifest.61ab99389a6f41f9fc3cb87f2eab875c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f4faaaf0ee15983ad84a644ee37724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c992bba816f4d4be43d",
+    "url": "/static/css/main.b5adea53.chunk.css"
+  },
+  {
+    "revision": "d34ffa71c28263307371",
+    "url": "/static/js/2.14ec9f19.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.14ec9f19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c992bba816f4d4be43d",
+    "url": "/static/js/main.04fdf147.chunk.js"
+  },
+  {
+    "revision": "dde6df0ed59d1126e71d",
+    "url": "/static/js/runtime-main.3c1de91f.js"
+  }
+]);
