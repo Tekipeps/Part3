bb1cfbfa3522b90dbd5227dd69c8d2017684f6ab
Tekena <tekipeps@localhost.localdomain>
2020-05-12 20:48:19 +0100

    working with mongodb

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
 const express = require("express");
+const app = express();
+const Person = require("./models/person");
 const morgan = require("morgan");
 const cors = require("cors");
-const app = express();
 
+app.use(express.static("build"));
 app.use(express.json());
 app.use(cors());
-app.use(express.static("build"));
 morgan.token("body", (req, res) => {
   return JSON.stringify(req.body);
 });
@@ -13,34 +15,23 @@ app.use(
   morgan(":method :url :status :req[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
-
+const errorHandler = (error, req, res, next) => {
+  // console.log(error.message);
+  if (error.name === "CastError" && error.kind === "ObjectId") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+  next(error);
+};
+app.use(errorHandler);
 app.get("/", (req, res) => {
   res.send("<h1>Phonebook backend</h1>");
 });
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
+app.get("/api/persons", (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      res.json(persons);
+    })
+    .catch((error) => next(error));
 });
 app.get("/info", (req, res) => {
   const date = new Date();
@@ -48,28 +39,31 @@ app.get("/info", (req, res) => {
     `<p>Phonebook has info for ${persons.length} people </p> <p>${date}</p>`
   );
 });
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(400).end();
-  }
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person.toJSON());
+      } else {
+        res.status(400).end();
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(400).send({ error: "malformatted id" });
+    });
 });
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  if (persons.find((person) => person.id === id)) {
-    persons = persons.filter((person) => person.id !== id);
-    res.status(204).end();
-  } else {
-    res.status(404).end();
-  }
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndRemove(req.params.id)
+    .then((result) => {
+      res.status(204).end();
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
-const generateId = () => {
-  return Math.round(Math.random() * 1000000000);
-};
-app.post("/api/persons", (req, res) => {
+
+app.post("/api/persons", (req, res, next) => {
   const body = req.body;
   if (
     body.name === "" ||
@@ -80,26 +74,21 @@ app.post("/api/persons", (req, res) => {
     res.status(400).json({
       error: "name or number missing",
     });
-  } else if (
-    persons.find(
-      (person) => person.name.toLowerCase() === body.name.toLowerCase()
-    )
-  ) {
-    res.status(400).json({
-      error: "name must be unique",
-    });
-  } else {
-    const person = {
-      name: body.name,
-      number: body.number,
-      id: generateId(),
-    };
-    persons = [...persons, person];
-    res.json(person);
   }
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  });
+
+  person
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson.toJSON());
+    })
+    .catch((error) => next(error));
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const password = process.argv[2];
+const name = process.argv[3];
+const number = process.argv[4];
+
+const url = `mongodb+srv://tekipeps:${password}@cluster0-8ksjd.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+console.log("connecting to", url);
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log("connected to mongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to mongoDB: ", error.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+if (process.argv.length < 4) {
+  Person.find({}).then((result) => {
+    console.log("phonebook:");
+    result.forEach((person) => {
+      console.log(person.name, person.number);
+    });
+    mongoose.connection.close();
+  });
+} else {
+  const person = new Person({
+    name: name,
+    number: number,
+  });
+  person.save().then((response) => {
+    console.log(`added ${name} number ${number} to phonebook`);
+    mongoose.connection.close();
+  });
+}
